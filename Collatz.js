dbca2ac7c2e18fe4afb7368a0e73921f03318ed5
NanoMars <Armand.packham@gmail.com>
2024-08-27 21:53:07 +1000

    mod variable to change different things

--- Collatz.js
+++ Collatz.js
@@ -1,16 +1,29 @@
 //Play with these variables to change the look!
 
 const SIZE = 3
-const CURVYNESS = 1451
-const COUNT = 854
+const CURVYNESS = 1873
+const COUNT = 121
+const MOD = 2 // Integar, range between 0 & 2
 
 function collatz(n, t) {
-    t.forward(SIZE)
+    if (MOD === 0) {
+          t.forward(SIZE)
+    } else if (MOD === 2) {
+          t.forward(SIZE * n)
+    }
     if (n % 2 === 0) {
-        t.left(CURVYNESS / Math.sqrt(n))
+        if (MOD === 1) {
+          t.left(CURVYNESS / Math.sqrt(n))
+        } else {
+          t.left(CURVYNESS)
+        }
         return (n / 2)
     } else {
-        t.right(CURVYNESS / Math.sqrt(n))
+        if (MOD === 1) {
+          t.right(CURVYNESS / Math.sqrt(n))
+        } else {
+          t.right(CURVYNESS)
+        }
         return (3*n + 1)
     }
    
